966a6548b79ba308d9678e589c04d47a3c9e332e
Kamanashis-Biswas <kamanashisbiswas79@gmail.com>
2021-02-27 02:32:56 +0600

    Firebase Setup

--- app/src/main/java/com/example/videomeeting/activities/SignInActivity.java
+++ app/src/main/java/com/example/videomeeting/activities/SignInActivity.java
@@ -1,8 +1,10 @@
-package com.example.videomeeting;
+package com.example.videomeeting.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.example.videomeeting.R;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        findViewById(R.id.textSignUp).setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+
+
     }
 }
--- app/src/main/java/com/example/videomeeting/activities/SignUpActivity.java
+++ app/src/main/java/com/example/videomeeting/activities/SignUpActivity.java
@@ -1,8 +1,10 @@
-package com.example.videomeeting;
+package com.example.videomeeting.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.videomeeting.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.imageBack).setOnClickListener(view -> onBackPressed());
+        findViewById(R.id.textSignIn).setOnClickListener(view -> onBackPressed());
     }
 }
