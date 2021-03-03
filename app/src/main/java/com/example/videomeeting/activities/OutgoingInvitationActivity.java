01cb0c3496b655553752b55347d8d20a30802470
Kamanashis-Biswas <kamanashisbiswas79@gmail.com>
2021-03-04 01:19:19 +0600

    token

--- app/src/main/java/com/example/videomeeting/activities/OutgoingInvitationActivity.java
+++ app/src/main/java/com/example/videomeeting/activities/OutgoingInvitationActivity.java
@@ -1,21 +1,52 @@
 package com.example.videomeeting.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.videomeeting.R;
 import com.example.videomeeting.models.User;
+import com.example.videomeeting.network.ApiClient;
+import com.example.videomeeting.network.ApiService;
+import com.example.videomeeting.utilities.Constants;
+import com.example.videomeeting.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class OutgoingInvitationActivity extends AppCompatActivity {
+
+    private PreferenceManager preferenceManager;
+    private String inviterToken = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outgoing_invitation);
+
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (task.isSuccessful() && task.getResult() != null){
+                    inviterToken = task.getResult().getToken();
+                }
+            }
+        });
+
 
         ImageView imageMeetingType = findViewById(R.id.imageMeetingType);
         String meetingType = getIntent().getStringExtra("type");
@@ -39,5 +70,56 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
 
         ImageView imageStopInvitation = findViewById(R.id.imageStopInvitation);
         imageStopInvitation.setOnClickListener(view -> onBackPressed());
+    }
+
+    private void initiateMeeting(String meetingType, String receiverToken){
+        try {
+
+            JSONArray tokens = new JSONArray();
+            tokens.put(receiverToken);
+
+            JSONObject body = new JSONObject();
+            JSONObject data = new JSONObject();
+
+            data.put(Constants.REMOTE_MSG_TYPE, Constants.REMOTE_MSG_INVITATION);
+            data.put(Constants.REMOTE_MSG_MEETING_TYPE, meetingType);
+            data.put(Constants.KEY_FIRST_NAME, preferenceManager.getString(Constants.KEY_FIRST_NAME));
+            data.put(Constants.KEY_LAST_NAME, preferenceManager.getString(Constants.KEY_LAST_NAME));
+            data.put(Constants.KEY_EMAIL, preferenceManager.getString(Constants.KEY_EMAIL));
+            data.put(Constants.REMOTE_MSG_INVITER_TOKEN, inviterToken);
+
+            body.put(Constants.REMOTE_MSG_DATA, data);
+            data.put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens);
+
+
+        }catch (Exception exception){
+            Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private void sendRemoteMessage(String remoteMessageBody, String type){
+        ApiClient.getClient().create(ApiService.class).sendRemoteMessage(
+                Constants.getRemoteMessageHeaders(), remoteMessageBody
+        ).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call,@NonNull Response<String> response) {
+                if (response.isSuccessful()){
+                    if (type.equals(Constants.REMOTE_MSG_INVITATION)){
+                        Toast.makeText(OutgoingInvitationActivity.this, "Invitation send successfully", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(OutgoingInvitationActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call,@NonNull Throwable t) {
+                Toast.makeText(OutgoingInvitationActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
